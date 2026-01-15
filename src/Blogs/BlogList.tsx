955d981df375a863a4e9755fb24a81258360edbe
fabrosjhordan-cmd <fabrosjhordan@gmail.com>
2026-01-15 20:24:28 +0800

    Backend APIs with supabase (Dynamic Slicer)

--- src/Blogs/BlogList.tsx
+++ src/Blogs/BlogList.tsx
@@ -1,27 +1,27 @@
-import { useDispatch } from "react-redux"
-import { useAppSelector } from "../hooks"
-import { deletePost, hidePost } from "../PostSlice"
-import { FaEyeSlash, FaRegTrashAlt } from "react-icons/fa";
+import { useAppDispatch, useAppSelector } from "../hooks"
+import {  FaRegTrashAlt } from "react-icons/fa";
+import { deletePost, fetchData } from "../PostSlice";
+import { useEffect } from "react";
 
 export const BlogList = () =>{
-    const posts = useAppSelector((state) => state.posts.posts);
-    const dispatch = useDispatch()
+    const posts = useAppSelector((state) => state.posts.items);
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(fetchData())
+    }, [])
 
     const handleDeletePost = (id: number) =>{
         dispatch(deletePost(id))
-    }
-
-    const handleHidePost = (id: number )=>{
-        dispatch(hidePost(id))
     }
 
     return (
         <>
         <div className="w-full items-center">
             {posts.map((post)=> (
-                <div className='flex-1 w-full h-full border-1 my-2 gap-4 p-2 items-center justify-between rounded-xl' hidden={post.hidden}>
+                <div key={post.id} className='flex-1 w-full h-full border-1 my-2 gap-4 p-2 items-center justify-between rounded-xl' hidden={post.hidden}>
                     <div className="font-bold text-3xl my-1">
-                        <h1 key={post.id}>
+                        <h1>
                             {post.subject}
                         </h1>
                     </div>
@@ -31,7 +31,7 @@ export const BlogList = () =>{
                         </p>
                     </div>
                     <div className="flex flex-row gap-2 mt-4">
-                        <button onClick={()=>handleHidePost(post.id)} className="flex flex-row items-center gap-2 border-1 border-zinc-500 px-2 py-1 rounded-lg hover:opacity-85 hover:cursor-pointer hover:scale-105 transition-300"><FaEyeSlash />Hide</button>
+                        {/* <button onClick={()=>handleHidePost(post.id)} className="flex flex-row items-center gap-2 border-1 border-zinc-500 px-2 py-1 rounded-lg hover:opacity-85 hover:cursor-pointer hover:scale-105 transition-300"><FaEyeSlash />Hide</button> */}
                         <button onClick={()=>handleDeletePost(post.id)} className="flex flex-row items-center gap-2 border-1 border-zinc-500 bg-red-500 text-white px-2 py-1 rounded-lg hover:opacity-85 hover:cursor-pointer hover:scale-105 transition-300"><FaRegTrashAlt />Delete</button>
                     </div>
                 </div>
--- src/PostSlice.ts
+++ src/PostSlice.ts
@@ -1,51 +1,73 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit"
-import { lorem } from "./data/dummydata"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { supabase } from "./supabaseClient"
 
-type Posts= {
-    id: number,
-    user: string,
-    subject: string,
-    body: string,
-    hidden: boolean
-}
 
-type PostsState = {
-    posts: Posts[]
-}
 
-const initialState: PostsState = {
-    posts: [
-        {id: 1, user: 'Jhordan',subject: 'New', body: lorem.subject, hidden: false}, {id: 2, user: 'Not Dan', subject: 'Newer', body: 'Newer Dummy data 2', hidden: false}
-    ]
-}
+export const fetchData = createAsyncThunk(
+    'posts/addFetch',
+    async (_, thunkAPI)=>{
+        try {
+            const { data, error } = await supabase.from('blogs').select()
+            if(error) throw error
+            return data
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error);
+        }
+    }
+)
+
+export const addPost = createAsyncThunk(
+    'posts/addPost',
+    async (post: {subject: string}, thunkAPI) =>{
+        try{
+            const {data, error} = await supabase.from('blogs').insert(post).select().single()
+            if(error) throw error
+            return data
+        }catch(error){
+            return thunkAPI.rejectWithValue(error);
+        }
+
+    }
+)
+
+export const deletePost = createAsyncThunk(
+    'post/deletepost',
+    async(id: number, thunkAPI) =>{
+        try{
+            const { error} = await supabase.from('blogs').delete().eq('id', id)
+            if(error) throw error
+            return id
+        }catch(error){
+            return thunkAPI.rejectWithValue(error);
+        }
+
+    }
+)
 
 const postSlice = createSlice({
     name: 'posts',
-    initialState,
-    reducers: {
-        addPost: (state, action: PayloadAction<string>) =>{
-            const newId = state.posts.length ? state.posts[state.posts.length -1 ].id + 1 : 1;
-            const newPost = {id: newId, user: action.payload, subject: action.payload, body: action.payload, hidden: false}
-            state.posts.push(newPost)
-        },
-
-        updatePost: (state, action: PayloadAction<string>) =>{
-            
-        },
-
-        deletePost: (state, action: PayloadAction<number>) =>{
-            state.posts = state.posts.filter((post) => post.id !== action.payload)
-        },
-        hidePost: (state, action: PayloadAction<number>) =>{
-            const hidePost = state.posts.find(
-                (post)=> post.id === action.payload
+    initialState: {items: [], loading: false, error: null},
+    reducers: {},
+    extraReducers: (builders) =>{
+        builders
+        .addCase(fetchData.pending, state =>{
+            state.loading = true
+        })
+        .addCase(fetchData.fulfilled, (state, action) =>{
+            state.loading = false
+            state.items = action.payload
+        })
+        .addCase(addPost.fulfilled, (state, action) =>{
+            state.loading = false
+            state.items = action.payload
+        })
+        .addCase(deletePost.fulfilled, (state, action )=>{
+            state.loading = false
+            state.items = state.items.filter(
+                post => post.id !== action.payload
             )
-            if(hidePost){
-                hidePost.hidden = !hidePost.hidden
-            }
-        }   
+        })
     }
 })
 
-export const {addPost, updatePost, deletePost, hidePost} = postSlice.actions
 export default postSlice.reducer
